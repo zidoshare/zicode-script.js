753b0813ff8e925820eb526fd2c30630f3f58ec2
zido <wuhongxu1208@gmail.com>
2018-06-04 17:07:58 +0800

    read from list check null

--- src/java/readFromList.js
+++ src/java/readFromList.js
@@ -41,21 +41,25 @@ public static ${className} ${opt.funcName}(List<${opt.valueType}> list,String...
         return obj;
     }
     for (String field : fields) {
+        String s = list.get(index++);
+        if(s == null){
+          continue;
+        }
         switch (field) {
 `
   template = template + properties.map(prop => {
     if (opt.valueType == 'Object') {
       return `case "${prop.name}":
-    obj.${prop.name} = list.get(index++);
+    obj.${prop.name} = s;
     break;`
     } else {
       var str = `case "${prop.name}":\n`
       if (prop.type == 'String') {
-        str += `obj.${prop.name} = list.get(index++);\n`
+        str += `obj.${prop.name} = s;\n`
       } else if (prop.type == 'Date') {
-        str += `obj.${prop.name} = new Date(Long.valueOf(list.get(index++)));\n`
+        str += `obj.${prop.name} = new Date(Long.valueOf(s));\n`
       } else if (prop.type == 'Long' || prop.type == 'Integer' || prop.type == 'Byte' || prop.type == 'Float') {
-        str += `obj.${prop.name} = ${prop.type}.valueOf(list.get(index++));\n`
+        str += `obj.${prop.name} = ${prop.type}.valueOf(s);\n`
       }
       str += `break;`
       return str
